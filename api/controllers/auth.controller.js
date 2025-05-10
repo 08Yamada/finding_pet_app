1996c2ce5858605610ddbd388cd258934ad7be6e
08Yamada <kameropon4949@gmail.com>
2025-05-10 23:47:21 +0900

    add /auth/register api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,32 @@
-export const register = (req,res)=>{
+import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
+
+export const register = async (req,res)=>{
+    const {username, email, pass} = req.body;
+
+    // HASH THE PASSWORD
+
+    const hashedPassword = await bcrypt.hash(pass, 10);
+
     //db operations
-    console.log("register endpoint")
-}
+    // console.log(req.body)
+    console.log(hashedPassword)
+
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+        data:{
+            username,
+            email,
+            password: hashedPassword,
+        },
+    });
+    const {password, ...others} = newUser
+    console.log(newUser)
+    res.json(others)
+};
 export const login = (req,res)=>{
     //db operations
-}
+};
 export const logout = (req,res)=>{
     //db operations
-}
+};
