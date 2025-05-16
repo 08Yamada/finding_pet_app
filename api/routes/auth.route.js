4dfd9e8ed1e9a43edaa1d8aaf44e07272048c1e8
08Yamada <kameropon4949@gmail.com>
2025-05-16 17:49:28 +0900

    fix: controllers/routes api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,7 +3,7 @@ import {login, logout, register} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/register", register);//getにする理由
+router.post("/register", register);//getにする理由
 
 router.post("/login", login);
 
