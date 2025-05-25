4ef92b526c412ff8b3ae72b47cc37c0cf2bed5ef
kamepaku <bangisaikyo600@gmail.com>
2025-05-25 12:49:14 +0900

    add: client folder from extarnal source

---
+++ client/src/routes/homePage/homePage.jsx
@@ -0,0 +1,43 @@
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+import { AuthContext } from "../../context/AuthContext";
+
+function HomePage() {
+
+  const {currentUser} = useContext(AuthContext)
+
+  // console.log(currentUser)
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find the animals</h1>
+          <p>
+            こちら葛飾区亀有公園前派出所
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
