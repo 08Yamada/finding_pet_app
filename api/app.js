0354e7c751663d17cc6ae712fc1c9342145bfdbe
08Yamada <[kameropon4949@gmail.com]>
2025-06-12 08:15:36 +0900

    fix: app.js

--- api/app.js
+++ api/app.js
@@ -3,26 +3,25 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
-import postRoute from "./routes/post.route.js";               
-import testRoute from "./routes/test.route.js";               
-import chatRoute from "./routes/chat.route.js";               
-import messageRoute from "./routes/message.route.js";               
+import postRoute from "./routes/post.route.js";
+import testRoute from "./routes/test.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}));//cookieを許す
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true })); //cookieを許す
 app.use(express.json());
 app.use(cookieParser());
 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
-app.use("/api/auth", authRoute)
-app.use("/api/users", userRoute)
-app.use("/api/posts", postRoute)
-app.use("/api/test", testRoute)
-app.use("/api/chats", chatRoute)
-app.use("/api/messages", messageRoute)
-
-app.listen(8800, ()=>{
-    console.log("Server is running!");
-
-})
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, 8800, () => {//動的にするためにPOSTを使う
+  console.log(`Server is running on port ${PORT}`);
+});
